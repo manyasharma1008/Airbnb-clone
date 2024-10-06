2785a6af53b279b86e63a87efc8e3531d43180ce
manyasharma1008 <manya.s1008@gmail.com>
2024-10-06 19:57:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+
+// Add any dynamic JavaScript functionality here if required
+
+document.querySelector('.explore-button').addEventListener('click', function () {
+    alert('Let’s explore some flexible options!');
+});
